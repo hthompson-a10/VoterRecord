bdbd4956d9c6ca166d8a5a38fccd1bda89611eae
Hunter Thompson <thompson.grey.hunter@gmail.com>
2020-09-15 05:08:37 -0700

    Fixed least and most similar function

--- src/VoterRecord.jl
+++ src/VoterRecord.jl
@@ -1,27 +1,47 @@
 module VoterRecord
+using DataStructures
 
 function policy_compare(senator_a, senator_b, senator_map, vote_deviation) 
     return vote_deviation[senator_map[senator_a], senator_map[senator_b]]
 end
 
 function most_similar(senator, senator_map, vote_deviation)
+    max_similar = -5000
+    sen_name = nothing
+    senator_pos = senator_map[senator]
+
+    i = 1
+    while i < size(vote_deviation[senator_pos, :], 1)
+        vote_diff = vote_deviation[senator_pos, i]
+        if vote_diff > max_similar && vote_diff != vote_deviation[senator_pos, senator_pos]
+            max_similar = vote_diff
+            sen_name = iterate(senator_map, i)[1].first
+        end
+        i = i + 1;
+    end
+    return sen_name
 end
 
 function least_similar(senator, senator_map, vote_deviation)
-end
+    min_similar = 5000
+    sen_name = nothing
+    senator_pos = senator_map[senator]
 
-function average(senator, senator_set, senator_map, vote_deviation)
-end
-
-function find_average_record(senator_set, senator_map, vote_deviation)
-end
-
-function bitter_rivals(vote_deviation)
+    i = 1
+    while i < size(vote_deviation[senator_pos, :], 1)
+        vote_diff = vote_deviation[senator_pos, i]
+        if vote_diff < min_similar
+            min_similar = vote_diff
+            sen_name = iterate(senator_map, i)[1].first
+        end
+        i = i + 1;
+    end
+    return sen_name
 end
 
 function main()
     entry_array = readlines("voting_record_dump109.txt")
-    senator_map = Dict()
+    senator_map = OrderedDict{String, Int}()
     sen_vote_matrix = Array{Int64}(undef, 0, 46) 
     cnt = 1
     for entry in entry_array
@@ -32,7 +52,10 @@ function main()
         cnt = cnt + 1
     end
     vote_deviation = sen_vote_matrix*sen_vote_matrix'
-    print(policy_compare("Akaka", "Alexander", senator_map, vote_deviation))
+
+    println(policy_compare("Akaka", "Alexander", senator_map, vote_deviation))
+    println(most_similar("Chafee", senator_map, vote_deviation))
+    println(least_similar("Santorum", senator_map, vote_deviation))
 end
 
 main()
